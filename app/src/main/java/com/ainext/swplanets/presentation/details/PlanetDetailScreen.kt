8fae3cd2a8c187e8a66b18a8479f7d57d1a0a813
Amal Ranasinghe <amal@soho.com.au>
2025-06-21 02:40:42 +0530

    Details : added big image

--- app/src/main/java/com/ainext/swplanets/presentation/details/PlanetDetailScreen.kt
+++ app/src/main/java/com/ainext/swplanets/presentation/details/PlanetDetailScreen.kt
@@ -1,7 +1,11 @@
 package com.ainext.swplanets.presentation.details
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
@@ -14,8 +18,11 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberAsyncImagePainter
+import com.ainext.swplanets.data.core.NetworkConstants.IMAGE_URL
 import com.ainext.swplanets.domain.Planet
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -47,6 +54,17 @@ fun PlanetDetailScreen(navController: NavController, planet: Planet) {
                 .padding(innerPadding)
                 .padding(16.dp)
         ) {
+            val imageUrl = IMAGE_URL + planet.name
+
+            Image(
+                painter = rememberAsyncImagePainter(imageUrl),
+                contentDescription = null,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(250.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.height(16.dp))
             Text("Climate: ${planet.climate}", style = MaterialTheme.typography.bodyLarge)
             Text("Gravity: ${planet.gravity}", style = MaterialTheme.typography.bodyMedium)
             Text(
